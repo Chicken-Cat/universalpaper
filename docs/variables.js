b213ab4159e7e6340b60b68e0bd95c79c5e3fc67
Chicken-Cat <132946622+Chicken-Cat@users.noreply.github.com>
2025-09-12 13:18:11 -0400

    Update and rename main to variables.js

---
+++ docs/variables.js
@@ -0,0 +1,15 @@
+var paper = 0; //paper in inventory
+var allPaper = 0; //all paper ever made, including sold
+var paperSold = 0; //paper sold in total
+var margin = 2.5; //cost per package
+var money = 0;
+var wood = 1000; //1 wood = 1 paper
+var woodCost = 10 //per 1000 wood
+var paperMaker1 = 0; //auto sheeter
+var paperMaker2 = 0; //mega sheeter
+var paperMaker3 = 0; //super sheeter
+var maker1Cost = 0; //1.2 - 1.3x per upgrade maybe?
+var maker2Cost = 0; //1.4 - 1.6?
+var maker3Cost = 0; // 1.8 - 2.2x?
+var paperRate = 0; //automatic paper makers ONLY
+var demand = 1;
