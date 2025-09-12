1b49a8118eb0786bfb088b366b5c4c276581ab55
Chicken-Cat <132946622+Chicken-Cat@users.noreply.github.com>
2025-09-12 13:21:11 -0400

    Create hidden.js

---
+++ docs/hidden.js
@@ -0,0 +1,6 @@
+// the fact you are seeing this means you're going through my code
+// i expected this so ill leave some comments throughout, 
+// they'll explain what i was hoping something would do, future ideas, that stuff
+// maybe some hidden stuff too... who knows?
+// anyways my discord user is chicken_cat_2 
+// tell me if you found this or smth idk man
