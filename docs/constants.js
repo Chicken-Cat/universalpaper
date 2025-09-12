934a538da0149c948a58b8ed3532ac62876271c4
Chicken-Cat <132946622+Chicken-Cat@users.noreply.github.com>
2025-09-12 13:19:23 -0400

    Create constants.js

---
+++ docs/constants.js
@@ -0,0 +1,3 @@
+// stuff that is ALWAYS happening
+// paper makers, time played, random events
+// those go HERE so i can stay organized and for you, my beloved player who wanted to look through my code
